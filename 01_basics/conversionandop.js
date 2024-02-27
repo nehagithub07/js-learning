3b915c24b8a7b996916f4cbeb45360643c4fe198
Neha <120331352+nehagithub07@users.noreply.github.com>
2024-02-27 15:50:30 +0000

    01_basics

--- 01_basics/conversionandop.js
+++ 01_basics/conversionandop.js
@@ -1,68 +1,30 @@
-let score = "hitesh"
-
+//let score = "33abc"
+//let score = null;
+//let score = undefined;
+//let score = true;
 //console.log(typeof score);
 //console.log(typeof(score));
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
-
+//let valueNumber = Number(score);
+//console.log(typeof valueNumber);
+//console.log(valueNumber);  // NaN -> not a number
 
 // "33" => 33
-// "33abc" => NaN
-//  true => 1; false => 0
+// "33abc" => Nan
+// true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+//let isloggedIn = 1
+//let isloggedIn = ""
+// let isloggedIn = "Neha"
+// let booleanisLoggedIn = Boolean(isloggedIn);
+// console.log(booleanisLoggedIn);
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
-
-// 1 => true; 0 => false
+// 1 => true; 0 => false 
 // "" => false
-// "hitesh" => true
+// "Neha" => true
 
 let someNumber = 33
 
-let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
-
-// *********************** Operations ***********************
-
-let value = 3
-let negValue = -value
-// console.log(negValue);
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
-
-let str1 = "hello"
-let str2 = " hitesh"
-
-let str3 = str1 + str2
-// console.log(str3);
-
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
-
-// console.log( (3 + 4) * 5 % 3);
-
-// console.log(+true);
-// console.log(+"");
-
-let num1, num2, num3
-
-num1 = num2 = num3 = 2 + 2
-
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
-
-// link to study
-// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
