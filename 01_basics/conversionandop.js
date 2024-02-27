8d43d1fd980300734474f5f93c1b0cebc54b793e
Neha <120331352+nehagithub07@users.noreply.github.com>
2024-02-27 16:09:22 +0000

    01_basics

--- 01_basics/conversionandop.js
+++ 01_basics/conversionandop.js
@@ -23,8 +23,48 @@
 // "" => false
 // "Neha" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***************** Operations **************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+// let str1 = "Hello"
+// let str2 = " Neha"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);  // o/p is 12
+// console.log(1 + "2"); // o/p is 12
+// console.log("1" + 2 + 2);  // bad way not recommanded o/p is 122 because we are not using pranthesis
+// console.log("1" + (2 + 2)); // O/P is 14
+// console.log(1 + 2 + "2");  // bad way not recommanded
+
+//console.log(+true);  // o/p is 1
+//console.log(true+); // error
+//console.log(+""); // o/p is 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2  // not recommanded
+
+let gameCounter = 100
+//gameCounter++; // 101
+++gameCounter;  // 101
+console.log(gameCounter);
