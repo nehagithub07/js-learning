576e3e3965806123ce70810da3bae0139cdb66c1
Neha <120331352+nehagithub07@users.noreply.github.com>
2024-03-08 16:11:32 +0000

    04_switch

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,35 @@
+// Switch
+//syntax
+
+// switch(key) {
+//     case value:
+        
+//         break;
+    
+//     default:
+//         break;
+// }
+
+// Example
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;  
+    default:
+        console.log("INVALID");
+        break;
+}
