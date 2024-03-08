fe1cb4d13a52f7c4ea102b6de6ffa3d228091dfd
Neha <120331352+nehagithub07@users.noreply.github.com>
2024-03-08 16:04:36 +0000

    04_ControlFlow

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,59 @@
+// if
+// if(condition ) {
+
+// }
+
+// Example
+// const temperature = 40
+// if(temperature < 50) { 
+//     console.log("Less than 50");
+// }
+// console.log("Temperature is greater than 50");
+
+
+// if-else
+
+// const temperature = 40
+// if(temperature < 50) { 
+//     console.log("Less than 50");
+// }
+// else {
+//     console.log("Temperature is greater than 50");
+
+// }
+
+// const score = 200
+// if(score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`); // error
+
+// ShortHand notation
+// const balance = 1000 
+// if(balance > 500) console.log("test"), 
+// console.log("test2");  // this code is not recommanded, bad practice
+
+//if-elseif
+// const balance = 1000 
+// if(balance < 500) {
+//     console.log("less than");
+// } else if(balance < 750) {
+//     console.log("lestt than 750")
+// } else {
+//     console.log("less than 1200")
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if(userLoggedIn && debitCard) {
+//     console.log("Allow to buy course");
+// }
+
+// if(loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
