1bc9440ee57c458929bee22c8cabaf03670131b5
Neha <120331352+nehagithub07@users.noreply.github.com>
2024-03-13 19:10:52 +0000

    05_loops

--- 05_loops/newfile.js
+++ 05_loops/newfile.js
@@ -13,8 +13,34 @@
 // reduce method
 
 const myNums = [1, 2, 3]
-const myTotal = myNums.reduce(function (acc, currVal) {
-    console.log(`acc: ${acc} and currval: ${currVal}`)
-    return acc + currVal
-}, 0)
-console.log(myTotal);
+// const myTotal = myNums.reduce(function (acc, currVal) {
+//     console.log(`acc: ${acc} and currval: ${currVal}`)
+//     return acc + currVal
+// }, 0)
+// console.log(myTotal);
+
+// const myTotal = myNums.reduce( (acc, currVal) => acc + currVal, 0)
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay);
