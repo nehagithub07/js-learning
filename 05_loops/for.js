59e432c47020f06cb0f8d5197275b7572f4d36d9
Neha <120331352+nehagithub07@users.noreply.github.com>
2024-03-13 18:34:00 +0000

    05_loops

---
+++ 05_loops/for.js
@@ -0,0 +1,37 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rd: 'ruby',
+    swift: 'swift by apple'
+}
+// for in
+// for (const key in myObject) {
+//   console.log(key);
+// }
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+//   }
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    console.log(key);  // in op it is giving the indexes 0 1 2 3 4
+}
+
+for (const key in programming) {
+    console.log(programming[key]);  
+}
+
+
+const map = new Map()  
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+// maps are not iterable
+
+for (const key in map) {
+    console.log(key);  
+}
--- 05_loops/forloop.js
+++ 05_loops/forloop.js
@@ -54,22 +54,60 @@
 
 // While loop
 
-let index = 0
-while (index <= 10) {
-    console.log(`Value of index is ${index}`);
-    index = index + 2;
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+// let myArr = ["flash", "batman", "superman"]
+// let arr = 0
+// while(arr < myArr.length) {
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr = arr + 1;
+// }
+
+// do while loop
+// let score = 1
+// do {
+//    console.log(`Score is ${score}`);
+//    score++;
+// } while (score <= 10);
+
+
+// for of loop
+const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`)
+// }
+
+// Maps
+const map = new Map()  // return unique values and store key value pairs
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India") // it will not print
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
 }
 
-let myArr = ["flash", "batman", "superman"]
-let arr = 0
-while(arr < myArr.length) {
-    console.log(`Value is ${myArr[arr]}`);
-    arr = arr + 1;
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
 }
 
+// for (const [key, value]  of myObject) {
+//     console.log(key, ':-', value); // TypeError: myObject is not iterable
+    
+// }  // object ko ese iterate nhi kr skte
 
-let score = 1
-do {
-   console.log(`Score is ${score}`);
-   score++;
-} while (score <= 10);
+
+
+
